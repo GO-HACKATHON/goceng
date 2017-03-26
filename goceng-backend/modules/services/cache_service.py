6cffad43b6b1bdc82c357dfb6256e101b9a24d8c
ananta pandu <anpandumail@gmail.com>
2017-03-26 08:15:55 +0700

    [backend] cache save to file

--- goceng-backend/modules/services/cache_service.py
+++ goceng-backend/modules/services/cache_service.py
@@ -1,10 +1,13 @@
 import json
+import os
+import pickle
 
 from config import Config
 from modules.helper import print_json, read_json, datetime_from_str, datetime_floor_hour, current_date
 
 class CacheService(object):
 
+  SAVE_DIR = '/tmp/'
   DICT = {}
   
   @staticmethod
@@ -14,7 +17,22 @@ class CacheService(object):
   @staticmethod
   def save (key, data):
     CacheService.DICT[key] = data
+    CacheService.save_to_disk()
   
   @staticmethod
   def if_any (key):
+    CacheService.load_from_disk()
     return key in CacheService.DICT
+
+  @staticmethod
+  def save_to_disk ():
+    filename = CacheService.SAVE_DIR + 'cache.pickle'
+    with open(filename, 'wb') as handle:
+      pickle.dump(CacheService.DICT, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+  @staticmethod
+  def load_from_disk ():
+    filename = CacheService.SAVE_DIR + 'cache.pickle'
+    if os.path.exists(filename):
+      with open(filename, 'rb') as handle:
+        CacheService.DICT = pickle.load(handle)
